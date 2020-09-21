5b0aba10015bc2c4064d81979c0177c870052ed5
Diego Felipe Orozco Penagos <Dioro1999@gmail.com>
2020-09-21 18:07:41 -0500

    flujos alternativos desarrollados y control de conexiones ahora se realiza una conexion/desconexion por cada llamado a un metodo

--- controladores/Producto.py
+++ controladores/Producto.py
@@ -1,33 +1,56 @@
 from flask import jsonify
 from db.conexion import conexion
+from mysql.connector.errors import IntegrityError,DatabaseError
 
 class Producto():
-	global cursor
-	cursor = conexion.cursor()
 		
 	def list():
-		lista = []
-		cursor.execute("SELECT Nombre, Unidades, Peso_unitario, Volumen_unitario, Periodo_caducidad, Precio_unitario, Tipo FROM producto")
-		rows = cursor.fetchall()
-		columns = [i[0] for i in cursor.description]
-		for row in rows:
-			registros = zip(columns,row)
-			json = dict(registros)
-			lista.append(json)
-		return jsonify(lista)
-		cursor.close()
-		conexion.close()
+		cnx = conexion()
+		cursor = cnx.cursor()
+		try:
+			lista = []
+			cursor.execute("SELECT Nombre, Unidades, Peso_unitario, Volumen_unitario, Periodo_caducidad, Precio_unitario, Tipo FROM producto")
+			rows = cursor.fetchall()
+			columns = [i[0] for i in cursor.description]
+			for row in rows:
+				registros = zip(columns,row)
+				json = dict(registros)
+				lista.append(json)
+			return jsonify(lista)
+		finally:
+			cursor.close()
+			cnx.close()
 
 	def create(body):
-		data = (body["nombre"],body["unidades"],body["peso"],body["volumen"],body["caducidad"],body["precio"],body["tipo"])
-		sql = "INSERT INTO producto (Nombre, Unidades, Peso_unitario, Volumen_unitario, Periodo_caducidad, Precio_unitario, Tipo, Imagen) VALUES(%s, %s, %s, %s, %s, %s, %s, DEFAULT);"
-		cursor.execute(sql,data)
-		conexion.commit()
-		#si no es un json simple lo que llega
-		#si llegan mas propiedades de las necesarias
-		#si llegan menos propiedades de las necesarias
-		#si los tipos de dato no son los esperados
-		#si las entradas no cumplen las restricciones de negocio
-		return {"estado":"insertado"}, 200
-		cursor.close()
-		conexion.close()
+		cnx = conexion()
+		cursor = cnx.cursor()
+		try:
+			#si llegan mas propiedades de las necesarias se ignoran
+			bodySchema = {
+				"nombre":body["nombre"],
+				"unidades":body["unidades"],
+				"peso":body["peso"],
+				"volumen":body["volumen"],
+				"caducidad":None,
+				"precio":body["precio"],
+				"tipo":body["tipo"],
+				"imagen":None
+			}
+			bodySchema.update(body)
+			data = (bodySchema["nombre"],bodySchema["unidades"],bodySchema["peso"],bodySchema["volumen"],bodySchema["caducidad"],bodySchema["precio"],bodySchema["tipo"],bodySchema["imagen"])
+			sql = "INSERT INTO producto (Nombre, Unidades, Peso_unitario, Volumen_unitario, Periodo_caducidad, Precio_unitario, Tipo, Imagen) VALUES(%s, %s, %s, %s, %s, %s, %s, %s);"
+			cursor.execute(sql,data)
+			cnx.commit()
+			return {"estado":"insertado"}, 200
+		except KeyError:
+			#si no es un json simple lo que llega o si llegan menos propiedades de las necesarias
+			return {"mensaje":"Propiedad necesaria no pasada"}, 400
+		except IntegrityError:
+			#si las entradas coinciden con un registro ya insertado
+			return {"mensaje":"Registro ya insertado"}, 422
+		except DatabaseError as e:
+			#si los tipos de entrada no son los esperados
+			return {"mensaje":"Restriccion de base de datos violada: " + str(e)}, 500
+		finally:
+			cursor.close()
+			cursor = cnx.cursor()
